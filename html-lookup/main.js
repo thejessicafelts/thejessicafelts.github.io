eb455e8084d6905d4210e3151746591852e1870a
Jessica Felts <thejessicafelts@gmail.com>
2020-09-21 21:59:35 -0400

    Adding feature to show/hide active tags

--- html-lookup/main.js
+++ html-lookup/main.js
@@ -1,16 +1,33 @@
-// SHOW/HIDE DEPRECATED TAGS
+// SHOW/HIDE ACTIVE AND DEPRECATED TAGS
 
+const active = document.querySelectorAll('.tag:not(.deprecated)');
 const deprecated = document.getElementsByClassName('deprecated');
+
 const showHideDeprecated = document.getElementById('show-deprecated');
+const showHideActive = document.getElementById('show-active');
+
+console.log(active);
 
 showHideDeprecated.addEventListener("click", function(){
     if(showHideDeprecated.checked){
-        for (var i=0; i<deprecated.length; i+=1){
+        for(var i=0; i<deprecated.length; i+=1){
             deprecated[i].style.display = "block";
         }
     } else {
-        for (var i=0; i<deprecated.length; i+=1){
+        for(var i=0; i<deprecated.length; i+=1){
             deprecated[i].style.display = "none";
+        }
+    }
+});
+
+showHideActive.addEventListener("click", function(){
+    if(showHideActive.checked){
+        for(var i=0; i<active.length; i+=1){
+            active[i].style.display = "block";
+        }
+    } else {
+        for(var i=0; i<active.length; i+=1){
+            active[i].style.display = "none";
         }
     }
 });
