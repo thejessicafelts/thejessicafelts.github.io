01e2a850d67ce83f205b0af965a74d91f7f57132
Jessica Felts <thejessicafelts@gmail.com>
2020-09-22 00:23:16 -0400

    Adding Show/Hide for Non-Standard Tags

--- html-lookup/main.js
+++ html-lookup/main.js
@@ -1,23 +1,30 @@
 // Show/Hide Checkbox Selections
-let selections = 2;
-
-// Deprecated Tags
-const deprecated = [...document.getElementsByClassName('deprecated')];
-const showHideDeprecated = document.getElementById('show-deprecated');
-showHideDeprecated.addEventListener("click", function(){
-    showHide(showHideDeprecated, deprecated)
-});
+let selections = 3;
 
 // Active Tags
-const active = [...document.querySelectorAll('.tag:not(.deprecated)')];
+const active = [...document.querySelectorAll('.tag:not(.deprecated):not(.non-standard)')];
 const showHideActive = document.getElementById('show-active');
 showHideActive.addEventListener("click", function(){
     showHide(showHideActive, active)
 });
 
+// Non-Standard Tags
+const nonStandard = [...document.querySelectorAll('.non-standard')];
+const showHideNonStandard = document.getElementById('show-non-standard');
+showHideNonStandard.addEventListener("click", function(){
+    showHide(showHideNonStandard, nonStandard)
+});
+
+// Deprecated Tags
+const deprecated = [...document.querySelectorAll('.deprecated')];
+const showHideDeprecated = document.getElementById('show-deprecated');
+showHideDeprecated.addEventListener("click", function(){
+    showHide(showHideDeprecated, deprecated)
+});
+
 // Generic Function
-function showHide(trigger, elements) {
-    if(trigger.checked) {
+function showHide(trigger, elements){
+    if(trigger.checked){
         selections++;
         elements.map(el => el.style.display = "block");
     } else {
